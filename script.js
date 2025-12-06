d3b9a5ad2fbc21d500c975def1a26aa91b069d8f
shanikatazmeen-design <shanikatazmeen@gmail.com>
2025-12-06 20:10:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function validate(e) {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('pass').value;
+    const age = document.getElementById('age').value;
+    const msgbBox = document.getElementById('message');
+    let message = '';
+
+    if (email === '') {
+
+        message = 'Please enter an email.';
+
+        msgBox.style.color = 'red';
+
+    } else if (pass === '') {
+
+        message = 'Password must be at least 8 characters.';
+
+        msgBox.style.color = 'red';
+
+    } else if (age === '') {
+
+        message = 'Age must be between 12 and 50.';
+
+        msgBox.style.color = 'red';
+
+    }
+
+    else {
+
+        message = 'Login successful!';
+
+        msgBox.style.color = 'green';
+
+    }
+
+    msgBox.innerText = message;
+
+}
